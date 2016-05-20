23f3128dc11971afdd56ee59fc2f7d174b9cf7f9
Ben Roa <benihanas1@gmail.com>
2016-05-20 14:37:43 -0700

    Passed all the tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,27 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+
+  $scope.data = {};
+
+  $scope.getAll = function() {
+    Links.getAll()
+    .then(function(resp) {
+      $scope.data.links = resp;
+    }).catch(function(err) {
+      console.log(err);
+    });
+  };
+
+  $scope.addOne = function(link) {
+    Links.addOne(link)
+    .then(function(resp) {
+      return resp.status;
+    }).catch(function(err) {
+      console.log(err);
+    });
+  };
+
+  $scope.getAll();
+
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,13 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+
+  $scope.link = {};
+
+  $scope.addLink = function(link) {
+    Links.addOne(link)
+      .then(function(resp) {
+        return resp.status;
+      });
+  };
 });
