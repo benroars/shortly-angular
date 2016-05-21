8c689a9f0af0dd8a4dbfb3f2bdf716de0d2cc491
Ben Roa <benihanas1@gmail.com>
2016-05-20 20:48:40 -0700

    Live validation for URLS

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,3 +1,5 @@
+var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
@@ -14,5 +16,13 @@ angular.module('shortly.shorten', [])
       });
   };
 
+  $scope.validate = function(url) {
+  	console.log(url);
+  	if(url.match(rValidUrl)){
+  		return 'VALID URL'
+  	} else {
+  		return 'NOT A VALID URL'
+  	}
+  }
 
 });
